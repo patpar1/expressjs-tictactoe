fbe5e5a9e0de028910750f441a3f6f4aaf6afd8b
patpar1 <59889312+patpar1@users.noreply.github.com>
2020-07-07 20:15:59 +0300

    Added Rahti support

--- app.js
+++ app.js
@@ -5,34 +5,137 @@ var logger = require("morgan");
 var Promise = require("bluebird");
 var bodyParser = require("body-parser");
 
-var indexRouter = require("./routes/index");
+Object.engine("html", require("ejs").renderFile);
 
 var app = express();
 
-var mongoose = require("mongoose");
-var dev_db_url = "mongodb://mongo:27017";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
-mongoose.Promise = Promise;
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
-app.use(logger("dev"));
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    mongoUser = process.env[mongoServiceName + "_USER"];
+
+    // If using env vars from secret from service binding
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length === 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length === 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = "mongodb://";
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+  }
+}
+
+var db = null,
+  dbDetails = new Object();
+
+var initDb = function(callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require("mongodb");
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = "MongoDB";
+
+    console.log("Connected to MongoDB at: %s", mongoURL);
+  });
+};
+
+app.use(logger("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+var indexRouter = require("./routes/index");
+
 app.use("/", indexRouter);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-var listener = app.listen(8080, function() {
-  console.log("Listening on port " + listener.address().port);
+app.get("/", function(req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err) {});
+  }
+  if (db) {
+    var col = db.collection("counts");
+    // Create a document with request IP and current time of request
+    col.insert({ ip: req.ip, date: Date.now() });
+    col.count(function(err, count) {
+      if (err) {
+        console.log("Error running count. Message:\n" + err);
+      }
+      res.render("index.html", { pageCountMessage: count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render("index.html", { pageCountMessage: null });
+  }
 });
+
+app.get("/pagecount", function(req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err) {});
+  }
+  if (db) {
+    db.collection("counts").count(function(err, count) {
+      res.send("{ pageCount: " + count + "}");
+    });
+  } else {
+    res.send("{ pageCount: -1 }");
+  }
+});
+
+// error handling
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something bad happened!");
+});
+
+initDb(function(err) {
+  console.log("Error connecting to Mongo. Message:\n" + err);
+});
+
+app.listen(port, ip);
+console.log("Server running on http://%s:%s", ip, port);
+
+module.exports = app;
